7805a7f29f9d6cc045ea39f4b8341e6a7d54069b
Peyman Pahlevani <peyman.phl@gmail.com>
2013-10-10 18:24:11 +0200

    new rasp

--- rasp.sh
+++ rasp.sh
@@ -4,7 +4,7 @@ echo -n "enter raspname"
 read -e rasp
 
 wget https://github.com/kapaA/nc4rasp/blob/master/batman-adv.ko
-wget https://github.com/kapaA/nc4rasp/blob/master/adhoc.sh
+wget raw.github.com/kapaA/nc4rasp/master/adhoc.sh
 sudo apt-get install batctl
 sudo apt-get install iw
 sudo chmod +x adhoc.sh
@@ -13,7 +13,7 @@ sudo sed -i '19a\cd /home/pi && bash sudo adhoc.sh' /etc/rc.local
 sudo sed -i '1d' /etc/hostname 
 echo myhost.com | sudo tee /etc/hostname
 echo $rasp | sudo tee /etc/hostname
-wget https://github.com/kapaA/nc4rasp/blob/master/hosts
+wget raw.github.com/kapaA/nc4rasp/master/hosts
 sudo rm /etc/hosts
 sudo cp hosts /etc/
 
